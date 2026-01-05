64421391155c749807689c60944144df90c16be5
Mehmet Emin <meminyaksi@gmail.com>
2026-01-06 00:06:02 +0300

    ilk commit

---
+++ Servo.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05.01.2026 23:43:09
+-- Design Name: 
+-- Module Name: Servo - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Servo is
+--  Port ( );
+end Servo;
+
+architecture Behavioral of Servo is
+
+begin
+
+
+end Behavioral;
